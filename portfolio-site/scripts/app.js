1b173ed844bbca73cd32228fde70baa708671335
claudiacole96 <claudia.evans@bryanuniversity.edu>
2022-01-20 13:37:26 -0500

    1/20

---
+++ portfolio-site/scripts/app.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active-slide");
+      slides[i].classList.add("not-active");
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].classList.remove("not-active");
+  slides[slideIndex-1].classList.add("active-slide");
+  dots[slideIndex-1].className += " active";
+}
